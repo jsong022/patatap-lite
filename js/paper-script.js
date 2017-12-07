b42139c1cf89ae3d016d79494940aee2766441b2
Jay Song <jsong022@ucr.edu>
2017-12-06 18:51:39 -0800

    added the audio files to github (sound functionality starts in this commit)

--- js/paper-script.js
+++ js/paper-script.js
@@ -1,8 +1,38 @@
 //array for all the circles to go into
 var circles = [];
 
+//array for all the sounds
+var sounds = [];
+
 //maximum point of the current canvas size (i.e. Bottom Right corner)
 var maxPt = new Point(view.size.width, view.size.height);
+
+sounds.push(new Howl({ src: ['./sounds/bubbles.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/clay.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/confetti.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/corona.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/dotted-spiral.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/flash-1.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/flash-2.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/flash-3.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/glimmer.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/moon.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/pinwheel.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/piston-1.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/piston-2.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/piston-3.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/prism-1.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/prism-2.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/prism-3.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/splits.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/squiggle.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/strike.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/suspension.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/timer.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/ufo.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/veil.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/wipe.mp3'] }));
+sounds.push(new Howl({ src: ['./sounds/zig-zag.mp3'] }));
 
 //on keyDown events
 function onKeyDown(event) {
@@ -11,8 +41,9 @@ function onKeyDown(event) {
 	var newCircle = new Path.Circle(Point.random() * maxPt, 100); 
 	newCircle.fillColor = randomColor();
 	circles.push(newCircle);
-	new Howl({ src: ['./sounds/bubbles.mp3'] }).play();
-};
+	sounds[0].play();
+	console.log(event.key)
+}
 
 //on each frame of the animation
 function onFrame(event){
